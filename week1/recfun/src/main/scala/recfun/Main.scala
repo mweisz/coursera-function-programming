c819d1e64566101657e2df298e992d3dd20df3ab
Michael Weisz <mail@michael-weisz.de>
2016-06-12 11:33:12 +0200

    Make gitignore only whitelist the source files.

---
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,40 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = 
+      if (c == 0 || r == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
+
+  /**
+   * Exercise 2 ())(
+   */
+  def balance(chars: List[Char]): Boolean = {
+      def loop(c: List[Char], openCount: Int): Boolean = 
+          if (c.isEmpty || openCount < 0) openCount == 0  
+          else if (c.head == '(') loop(c.tail, openCount + 1) 
+          else if (c.head == ')') loop(c.tail, openCount - 1) else loop(c.tail, openCount) 
+          
+      loop(chars, 0)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+      if (money == 0) 1 
+      else if (coins.isEmpty || money < 0 ) 0
+      else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
+}
